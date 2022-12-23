3e7f7abe29233d34063940360085c6ef71798ea0
Zedric Amiel Abadines <113886927+zaabadinesupou@users.noreply.github.com>
2022-12-23 17:18:54 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,7 +71,7 @@
                 <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
                     <div class="text-center text-sm text-gray-500 sm:text-left">
                         <div class="flex items-center">
-                            &copy; {{ date('Y') }} IS 238
+                            &copy; {{ date('Y') }} Zedric Amiel Abadines
                         </div>
                     </div>
 
